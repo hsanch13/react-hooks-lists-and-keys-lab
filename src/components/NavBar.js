8aa47537b81bc44ffb1ae7375519e62b8f3cbf6c
hsanch13 <hayleysanchez13@gmail.com>
2024-03-01 22:13:33 -0700

    added lists and keys to the NavBar, ProjectItem and ProjectList components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,19 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  //create a variable and iterate through the array 
+  //pass two callback functions, the array and index to give it a unique ID
+  //need to string interpolate 
+  //return the who function but calling the name of your variable in your component 
+
+  const linkTags = links.map((link, index) => {
+    return <a key={index} href={`#${link}`}>{link}</a>
+  })
+  return (
+  <nav>
+    {linkTags}
+  </nav>
+  );
 }
 
 export default NavBar;
